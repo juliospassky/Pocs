34e4c216151a476e776fc4db9251bb23e9202f43
jo55658 <juliocto2015@gmail.com>
2022-02-12 20:24:29 -0300

    :tada: First Commit

---
+++ arch-ms/Application/AdapterInbound/Rest/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.AdapterInbound.Rest
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Todo>> Get()
+        {
+            var todo = new Todo
+            {
+                Name = "Julio Oliveira",
+                Email = "juliocto2011@gmail.com"                
+            };
+
+            return Ok(todo);
+        }
+    }
+}
---
+++ arch-ms/ApplicationTests/AdapterInbound/Rest/TodoControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Application.AdapterInbound.Rest;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplicationTests.AdapterInbound.Rest
+{
+    public class TodoControllerTests
+    {
+        [Fact]
+        public async Task When_HTTPGetTodo_Expect_Todo()
+        {
+            // Arrange
+            var controller = new TodoController();
+
+            // Act
+            var actionResult = await controller.Get();
+            var result = actionResult.Result as OkObjectResult;
+
+            // Assert
+            Assert.Equal("", result.Value);
+        }
+    }
+}
