8bf623ca1a7667d5ccdbfd3c23c90782039aa556
jo55658 <juliocto2015@gmail.com>
2022-02-15 14:02:26 -0300

    :white_check_mark: :art: - CRUD and Postman Tests

--- arch-ms/Application/AdapterInbound/Rest/TodoController.cs
+++ arch-ms/Application/AdapterInbound/Rest/TodoController.cs
@@ -2,6 +2,7 @@
 using Domain.Services;
 using Domain.Common;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Entities.Common;
 
 namespace Application.AdapterInbound.Rest
 {
@@ -9,22 +10,46 @@ namespace Application.AdapterInbound.Rest
     [ApiController]
     public class TodoController : ControllerBase
     {
-
-        [HttpGet]
-        public async Task<ActionResult<Todo>> Get()
-        {
-            //await _service.Get
-
-            return Ok();
-        }
-
         [HttpPost]
-        public async Task<ActionResult<Todo>> PostTodo([FromServices] TodoService service, [FromBody] Todo todo)
+        public async Task<ActionResult<Todo>> Post([FromServices] TodoService service, [FromBody] Todo todo)
         {
             todo.Audit = new Audit("Julio Oliveira");
             await service.Create(todo);
 
-            return CreatedAtAction(nameof(PostTodo), todo);
+            return CreatedAtAction(nameof(Post), todo);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> Get([FromServices] TodoService service, string id)
+        {
+            var todo = await service.GetById(id);
+
+            return Ok(todo);
+        }
+
+        [HttpGet("query")]
+        public async Task<ActionResult<Todo>> GetPages([FromServices] TodoService service, [FromQuery] FilterBase _filters)
+        {
+            var todos = await service.GetPages(_filters.SkipSize, _filters.LimitSize);
+
+            return Ok(todos);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Todo>> Put([FromServices] TodoService service, [FromBody] Todo todo, string id)
+        {
+            await service.Update(id, todo);
+
+            return Ok(todo);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromServices] TodoService service, string id)
+        {
+            await service.Delete(id);
+
+            return Ok();
+        }
+
     }
 }
--- arch-ms/Domain/Services/TodoService.cs
+++ arch-ms/Domain/Services/TodoService.cs
@@ -7,27 +7,34 @@ namespace Domain.Services
 {
     public class TodoService
     {
-        private readonly IMongoCollection<Todo> _todoService;
+        private readonly IMongoCollection<Todo> _service;
         public TodoService(IOptions<MongoDbConfig> options)
         {
             var mongoDb = new MongoClient(options.Value.ConnectionString);
-            _todoService = mongoDb.GetDatabase(options.Value.DatabaseName)
+            _service = mongoDb.GetDatabase(options.Value.DatabaseName)
                 .GetCollection<Todo>(options.Value.Todo);
         }
 
-        public async Task<List<Todo>> GetAll() =>
-            await _todoService.Find(_ => true).ToListAsync();
 
-        public async Task<Todo> Get(string id) =>
-            await _todoService.Find(o => o.Id == id).FirstOrDefaultAsync();
+        public async Task<List<Todo>> GetPages(int skipSize, int limitSize)
+        {
+            return await _service
+                .Find(o => true)
+                .Skip(skipSize)
+                .Limit(limitSize)
+                .ToListAsync();
+        }
+
+        public async Task<Todo> GetById(string id) =>
+            await _service.Find(o => o.Id == id).FirstOrDefaultAsync();
 
         public async Task Create(Todo newTodo) =>
-            await _todoService.InsertOneAsync(newTodo);
+            await _service.InsertOneAsync(newTodo);
 
         public async Task Update(string id, Todo updateTodo) =>
-            await _todoService.ReplaceOneAsync(o => o.Id == id, updateTodo);
+            await _service.ReplaceOneAsync(o => o.Id == id, updateTodo);
 
         public async Task Delete(string id) =>
-            await _todoService.DeleteOneAsync(o => o.Id == id);
+            await _service.DeleteOneAsync(o => o.Id == id);
     }
 }
